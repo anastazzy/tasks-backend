120b1448512099a7965a96323cc57c92f4e77904
Waimagi <shitova.ap@spbgut.ru>
2021-07-19 13:21:26 +0300

    Added interface for viewing picture of Earth.

---
+++ WebApplication1/Controllers/EarthPictureController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using WebApplication1.Dto;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/earth-picture")]
+    [ApiController]
+    public class EarthPictureController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public EarthPictureController()
+        {
+            _client = new HttpClient();
+        }
+
+        [HttpGet]
+        public async Task<FileContentResult> Index([FromQuery]GetEarthPictureDto p)
+        {
+            var url = "https://api.nasa.gov/planetary/earth/imagery";
+            var urlParams = $"?api_key=Usw5Wr7QN8DFeKVpREGZI43uxrAYfQ6FoXGOEF34&lon={p.Lon}&lat={p.Lat}&date={p.Date}&dim={p.Dim}";
+            var response = await _client.GetAsync(url + urlParams);
+
+            return File(await response.Content.ReadAsByteArrayAsync(), "image/png");
+        }
+    }
+}
---
+++ WebApplication1/Dto/GetEarthPictureDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Dto
+{
+    public class GetEarthPictureDto
+    {
+        public string Lon { get; set; }
+        public string Lat { get; set; }
+        public string Date { get; set; }
+        public string Dim { get; set; }
+    }
+}
